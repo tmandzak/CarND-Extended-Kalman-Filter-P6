bcd183685a9f7f40b1de5c1af8bdb8cccd8305a5
Taras Mandzak <tmandzak@gmail.com>
2017-07-05 14:00:26 +0200

    Implementation of Rewiewer's improvement suggestions

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -94,8 +94,9 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
        */
       float ro = measurement_pack.raw_measurements_[0];
       float phi = measurement_pack.raw_measurements_[1];
-      //set the state with the initial location and zero velocity
-      ekf_.x_ << ro * cos(phi), ro * sin(phi), 0, 0;
+      float rodot = measurement_pack.raw_measurements_[2];
+      //set the state with the initial location and velocity (based on radial component only)
+      ekf_.x_ << ro * cos(phi), ro * sin(phi), rodot * cos(phi), rodot * sin(phi);
 
     } else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
       /**
@@ -127,48 +128,53 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   //compute the time elapsed between the current and previous measurements
   float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0; //dt - expressed in seconds
-  previous_timestamp_ = measurement_pack.timestamp_;
 
-  float dt_2 = dt * dt;
-  float dt_3 = dt_2 * dt;
-  float dt_4 = dt_3 * dt;
+  // when delta time gets a near zero value could cause some arithmetic inconsistency in the calculations, to solve that possible issues it's recommended including a test to prevent those situations
+  if ( dt>0.001 ){
 
-  //Modify the F matrix so that the time is integrated
-  ekf_.F_(0, 2) = dt;
-  ekf_.F_(1, 3) = dt;
+    previous_timestamp_ = measurement_pack.timestamp_;
 
-  //set the process covariance matrix Q
-  ekf_.Q_ << dt_4 / 4 * noise_ax, 0, dt_3 / 2 * noise_ax, 0,
-             0, dt_4 / 4 * noise_ay, 0, dt_3 / 2 * noise_ay,
-             dt_3 / 2 * noise_ax, 0, dt_2*noise_ax, 0,
-             0, dt_3 / 2 * noise_ay, 0, dt_2*noise_ay;
+    float dt_2 = dt * dt;
+    float dt_3 = dt_2 * dt;
+    float dt_4 = dt_3 * dt;
 
-  ekf_.Predict();
+    //Modify the F matrix so that the time is integrated
+    ekf_.F_(0, 2) = dt;
+    ekf_.F_(1, 3) = dt;
 
-  /*****************************************************************************
-   *  Update
-   ****************************************************************************/
+    //set the process covariance matrix Q
+    ekf_.Q_ << dt_4 / 4 * noise_ax, 0, dt_3 / 2 * noise_ax, 0,
+        0, dt_4 / 4 * noise_ay, 0, dt_3 / 2 * noise_ay,
+        dt_3 / 2 * noise_ax, 0, dt_2*noise_ax, 0,
+        0, dt_3 / 2 * noise_ay, 0, dt_2*noise_ay;
 
-  /**
+    ekf_.Predict();
+
+    /*****************************************************************************
+     *  Update
+     ****************************************************************************/
+
+    /**
    TODO:
-   * Use the sensor type to perform the update step.
-   * Update the state and covariance matrices.
-   */
+     * Use the sensor type to perform the update step.
+     * Update the state and covariance matrices.
+     */
 
-  if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
-    // Radar updates
-    ekf_.H_ = tools.CalculateJacobian(ekf_.x_);
-    ekf_.R_ = R_radar_;
-    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
-  } else {
-    // Laser updates
-    ekf_.H_ = H_laser_;
-    ekf_.R_ = R_laser_;
-    ekf_.Update(measurement_pack.raw_measurements_);
+    if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
+      // Radar updates
+      ekf_.H_ = tools.CalculateJacobian(ekf_.x_);
+      ekf_.R_ = R_radar_;
+      ekf_.UpdateEKF(measurement_pack.raw_measurements_);
+    } else {
+      // Laser updates
+      ekf_.H_ = H_laser_;
+      ekf_.R_ = R_laser_;
+      ekf_.Update(measurement_pack.raw_measurements_);
+    }
+
+    // print the output
+    cout << "x_ = " << ekf_.x_ << endl;
+    cout << "P_ = " << ekf_.P_ << endl;
   }
-
-  // print the output
-  cout << "x_ = " << ekf_.x_ << endl;
-  cout << "P_ = " << ekf_.P_ << endl;
 }
 
